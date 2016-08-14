bf5bcecdc78f46c17ac7a68fa1edaf9f07cbd2bd
giepe <giepe@onet.eu>
2016-08-14 13:20:45 +0200

    update

---
+++ src/main/java/messenger/resources/ProfileResource.java
@@ -0,0 +1,45 @@
+package messenger.resources;
+
+import messenger.model.Profile;
+import messenger.services.ProfileService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Piotrek on 28.06.15.
+ */
+@Path("/profiles")
+@Consumes(MediaType.APPLICATION_XML)
+@Produces(MediaType.APPLICATION_XML)
+public class ProfileResource {
+    ProfileService profileService = new ProfileService();
+
+    @GET
+    public List<Profile> getProfiles() {
+        return profileService.getAllProfiles();
+    }
+
+    @GET
+    @Path("/{profileName}")
+    public Profile getProfile(@PathParam("profileName") String profileName) {
+        return profileService.getProfile(profileName);
+    }
+
+    @POST
+    public String addProfile(Profile profile) {
+        return profileService.addProfile(profile);
+    }
+
+    @PUT
+    public String updateProfile(Profile profile) {
+        return profileService.updateProfile(profile);
+    }
+
+    @DELETE
+    public String deleteProfile(Profile profile) {
+        return profileService.deleteProfile(profile.getProfileName());
+    }
+}
